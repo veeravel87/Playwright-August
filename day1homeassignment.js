051aca8fa16c0952c4412d137b8ff69acaf784eb
veeravel87 <60774489+veeravel87@users.noreply.github.com>
2025-08-16 17:09:13 +0530

    Add files via upload
    Week1 day1 assignment added

---
+++ day1homeassignment.js
@@ -0,0 +1,23 @@
+
+
+var city="chennai"
+console.log("I live in "+ city+" and the Data type is :"  +typeof city)
+let marks=85
+console.log("Marks:"+(marks+10)+" and the Data type is :" +typeof marks)
+isWeekend=true
+
+if(isWeekend)
+{
+    console.log("Relax"+" and the Data type is:"+ typeof isWeekend)
+
+
+}
+else{
+    console.log("Work")
+}
+let futureGoal
+console.log(typeof futureGoal)
+
+const name="veera"
+console.log(name)
+console.log(typeof name)
